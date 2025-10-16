5f0ef16eeff6bb7bb9e0c83aaaad81f8080b30d3
Ruan Osório Schmidt <ruan.osorio@dbccompany.com.br>
2025-10-16 16:21:25 -0300

    Finalizando recuperação de todos os arquivos do projeto

---
+++ src/main/java/utils/BaseAPIConfig.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class BaseAPIConfig {
+    /*
+     Classe usada como base para a configuração URI
+     Em projetos com mais de um ambiente a opção seria utilizar um arquivo de properties para cada ambiente
+     */
+
+    public static final String  BASE_URI = "https://dog.ceo/api/";
+
+}
--- src/test/java/funcional/ValidarConsultasApiBreedsTest.java
+++ src/test/java/funcional/ValidarConsultasApiBreedsTest.java
@@ -16,17 +16,13 @@ public class ValidarConsultasApiBreedsTest {
 
         @Test
         @Description("Deve retornar uma lista de todas as raças de cachorros com sucesso")
-        public void testListarTodasRacas(){
+        public void testListarTodasRacas() {
             Response response = breedClient.getListaRacas();
             BreedsDTO breedsDTOResponse = response.as(BreedsDTO.class);
 
             assertThat(response.getStatusCode(), is(200));
             assertThat(breedsDTOResponse.getMessage(), notNullValue());
             assertThat(breedsDTOResponse.getStatus(), equalTo("success"));
-<<<<<<< HEAD
-=======
-
->>>>>>> e73f80f (criando a classe BreedsClient com os metodos GETs da API e a classe DTO para validação do response)
         }
 
 }
